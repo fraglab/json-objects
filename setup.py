c4b2a459988d4363376a1f9d4eb3a4f4f25cfb92
defat <defat@ukr.net>
2017-06-08 17:41:09 +0300

    Version increment

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     py_modules=['json_objects'],
     zip_safe=False,
     url='https://github.com/fraglab/json-objects',
-    download_url='https://github.com/fraglab/json-objects/archive/0.0.1.tar.gz',
+    download_url='https://github.com/fraglab/json-objects/archive/0.0.3.tar.gz',
     entry_points={
         'kombu.serializers': [
             'json_objects = json_objects:register_args'
